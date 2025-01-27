4c383f0d0fab60fe08f3fdc4ce1561266cafc4af
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2025-01-27 16:28:11 +0300

    Update fifth.java

--- Ocak/Works/fifth.java
+++ Ocak/Works/fifth.java
@@ -2,8 +2,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import javax.swing.text.StyledEditorKit.BoldAction;
-
 public class fifth{
     public static void main(String[] args) {
         List<List<Integer>> lists= Arrays.asList(
