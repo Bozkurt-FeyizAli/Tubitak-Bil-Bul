7490561a2a0ef7ec7303c681d2d276049e09d249
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2025-03-08 16:48:45 +0300

    Update Answer2_1.java

--- Şubat/Answer2_1.java
+++ Şubat/Answer2_1.java
@@ -116,3 +116,125 @@ public class Answer2_1 {
     }
 
     public static boolean baştanSondanToplamıVarMı(int[] arr, int st, int fn, int res){
+        if(res==0){ 
+            //System.out.println(st);
+            return true;}
+        if(arr[st]==res||arr[fn]==res) return true;
+        if(res<0) return false;
+        if(st>fn) return false;
+        if(st==fn&&arr[st]!=res) return false;
+        if(arr[st]>res) return false;
+        res-=(arr[st]+arr[fn]);
+        return baştanSondanToplamıVarMı(arr, st+1, fn-1, res);
+    }
+    public static boolean baştanSondanToplamıVarMı(int[] arr, int res) {
+        int st = 0;
+        int fn = arr.length - 1;
+    
+        while (st <= fn) {
+            //System.out.println(String.format("ilk: %d, son: %d", st, fn));
+            // Eğer res, baştan veya sondan bir elemana eşitse true döndür
+            if (arr[st] == res || arr[fn] == res) {
+                return true;
+            }
+    
+            // Eğer res, baştan ve sondan elemanların toplamından büyükse, baştan elemanı çıkar
+            if (arr[st] + arr[fn] < res) {
+                res -= arr[st];
+                st++;
+            }
+            // Eğer res, baştan ve sondan elemanların toplamından küçükse, sondan elemanı çıkar
+            else if (arr[st] + arr[fn] > res) {
+                res -= arr[fn];
+                fn--;
+            }
+            // Eğer res, baştan ve sondan elemanların toplamına eşitse true döndür
+            else {
+                return true;
+            }
+        }
+    
+        // Eğer döngü sonunda res değeri 0 ise true, değilse false döndür
+        return res == 0;
+    }
+    
+    public static ArrayList<ŞifreSonuç> sonuçVarMıKelime(String word, ŞifreSonuç[] şifreSonuçs){
+        ArrayList<ŞifreSonuç> results= new ArrayList<>();
+        char[] c=word.toCharArray();
+        for (ŞifreSonuç şifreSonuç : şifreSonuçs) {
+            int[] arr=new int[c.length];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i]=HarfSayiDonusum.harfToSayi(c[i]);
+            }
+            ötele(arr, şifreSonuç.şifre);
+            if(ilkNdeToplamıVarMı(arr, 0, şifreSonuç.sonuç))
+            //if(ilkNdeToplamıVarMı(arr, 0, şifreSonuç.sonuç)) 
+                results.add(şifreSonuç);
+        }
+        return results;
+    }
+
+    public static void ötele(int[] arr, String s){
+        if(s.length()>arr.length) return;
+        if(s.length()<arr.length){
+            while(s.length()<arr.length)
+                s+=s;
+            s=s.substring(0, arr.length);
+        }
+        else if(arr.length<s.length()){
+            int[] arr1= new int[s.length()];
+            for (int i = 0, j=0; i < arr1.length; i++, j++) {
+                if(j==arr.length) j=0;
+                arr1[i]=arr[j];
+            } 
+            arr=arr1;
+        }
+        int i=0;
+        for (char si : s.toCharArray()) {
+            arr[i]=arr[i]+si-48;
+            if(arr[i]>28) arr[i]-=28;
+            i++;
+            //System.out.println(arr[i-1]);
+        }
+    }
+
+}
+
+
+class ŞifreSonuç{
+    String şifre;
+    int sonuç;
+    public ŞifreSonuç(String şifre, int sonuç) {
+        this.şifre = şifre;
+        this.sonuç = sonuç;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return şifre+" "+Integer.toString(sonuç) ;
+    }
+    
+}
+
+
+class HarfSayiDonusum {
+    
+    // Türk alfabesindeki harflerin sırası
+    private static final String ALFABE = "abcçdefgğhiıjklmnoöprsştuüvyz";
+
+    // Sayıyı harfe dönüştürme fonksiyonu
+    public static char sayiToHarf(int sayi) {
+        return ALFABE.charAt(sayi);
+    }
+
+    // Harfi sayıya dönüştürme fonksiyonu
+    public static int harfToSayi(char harf) {
+        harf=Character.toLowerCase(harf);
+        return ALFABE.indexOf(harf);
+    }
+
+
+}
+
+
